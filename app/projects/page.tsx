8879ee294fc64083862c5b553f17dcb5f964eeff
FirePlank <44502537+FirePlank@users.noreply.github.com>
2024-11-01 18:17:54 +0200

    improved images and projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {motion} from "framer-motion";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
 import {BsArrowUpRight, BsGithub} from "react-icons/bs";
@@ -16,7 +16,7 @@ const projects = [
         title: 'Fitsmrt',
         description: 'An AI fitness trainer and planner mobile app that creates personalized workout routines, helps track progress and gives nutrition advice.',
         stack: [{name: 'React Native'}, {name: 'Supabase'}, {name: 'Node.js'}],
-        image: '/assets/projects/fitsmrt.png',
+        image: '/assets/projects/fitsmrt.webp',
         live: 'https://fitsmrt.com',
         github: 'https://github.com/fitsmrt'
     },
@@ -25,7 +25,7 @@ const projects = [
         title: 'Shortlang',
         description: 'A programming language designed for code golfing and competitive programming.',
         stack: [{name: 'Rust'}, {name: 'Interpreter'}, {name: 'Compiler'}],
-        image: '/assets/projects/shortlang.jpeg',
+        image: '/assets/projects/shortlang.webp',
         github: 'https://github.com/ShortLang/ShortLang'
     },
     {
@@ -42,100 +42,110 @@ const projects = [
         title: 'Hydrochess',
         description: 'A chess engine written in Rust, with a focus on speed and efficiency. It has a rating of 2000+ on Lichess.',
         stack: [{name: 'Rust'}, {name: 'Data Structures'}, {name: 'Algorithms'}],
-        image: '/assets/projects/chess.jpeg',
+        image: '/assets/projects/chess.webp',
         github: 'https://github.com/FirePlank/HydroChess'
     }
 ];
 
 const Projects = () => {
-    const [project, setProject] = useState(projects[0]);
+    const [projectIndex, setProjectIndex] = useState(0);
+    const [imageDimensions, setImageDimensions] = useState(projects.map(() => ({width: 0, height: 0})));
 
-    return <motion.section
-        initial={{opacity: 0}}
-        animate={{opacity: 1, transition: {duration: 0.25, delay: 0.4, ease: "easeIn"}}}
-        className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
-    >
-        <div className="container mx-auto">
-            <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-                <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-                    <div className="flex flex-col gap-[30px] h-[50%]">
-                        {/* Project number */}
-                        <div className="text-8xl leading-none font-extrabold text-transparent"
-                             style={{WebkitTextStroke: '1px white'}}
-                        >
-                            {project.num}
-                        </div>
-                        {/* Project title */}
-                        <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                            {project.title}
-                        </h2>
-                        {/* Project description */}
-                        <p className="text-white/60">
-                            {project.description}
-                        </p>
-                        {/* Project stack */}
-                        <ul className="flex gap-4">
-                            {project.stack.map((tech, index) => (
-                                <li key={index} className="text-xl text-accent">
-                                    {tech.name}
-                                    {index !== project.stack.length - 1 && <span className="text-white/60">,</span>}
-                                </li>
-                            ))}
-                        </ul>
-                        {/* Project border */}
-                        <div className="border border-white/20"/>
-                        {/* Project buttons */}
-                        <div className="flex items-center gap-4">
-                            {/* Live button */}
-                            {project.live && <Link href={project.live} target="_blank">
-                                <TooltipProvider delayDuration={100}>
-                                    <Tooltip>
-                                        <TooltipTrigger
-                                            className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                            <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>
-                                                Visit website
-                                            </p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                            </Link>
-                            }
-                            {/* GitHub button */}
-                            <Link href={project.github} target="_blank">
-                                <TooltipProvider delayDuration={100}>
-                                    <Tooltip>
-                                        <TooltipTrigger
-                                            className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                            <BsGithub className="text-white text-3xl group-hover:text-accent"/>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            <p>
-                                                View source
-                                            </p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </TooltipProvider>
-                            </Link>
+    useEffect(() => {
+        const loadImageDimensions = async () => {
+            const dimensions = await Promise.all(projects.map(project => {
+                return new Promise(resolve => {
+                    if (project.image.endsWith('.mp4')) {
+                        resolve({width: '100%', height: '100%'});
+                    } else {
+                        const img = new window.Image();
+                        img.src = project.image;
+                        img.onload = () => {
+                            resolve({width: img.width, height: img.height});
+                        };
+                    }
+                });
+            }));
+            setImageDimensions(dimensions);
+        };
+
+        loadImageDimensions();
+    }, []);
+
+    return (
+        <motion.section
+            initial={{opacity: 0}}
+            animate={{opacity: 1, transition: {duration: 0.25, delay: 0.4, ease: "easeIn"}}}
+            className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
+        >
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+                    <div
+                        className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+                        <div className="flex flex-col gap-[30px] h-[50%]">
+                            <div className="text-8xl leading-none font-extrabold text-transparent"
+                                 style={{WebkitTextStroke: '1px white'}}>
+                                {projects[projectIndex].num}
+                            </div>
+                            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                                {projects[projectIndex].title}
+                            </h2>
+                            <p className="text-white/60">
+                                {projects[projectIndex].description}
+                            </p>
+                            <ul className="flex gap-4">
+                                {projects[projectIndex].stack.map((tech, index) => (
+                                    <li key={index} className="text-xl text-accent">
+                                        {tech.name}
+                                        {index !== projects[projectIndex].stack.length - 1 &&
+                                            <span className="text-white/60">,</span>}
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="border border-white/20"/>
+                            <div className="flex items-center gap-4">
+                                {projects[projectIndex].live && (
+                                    <Link href={projects[projectIndex].live} target="_blank">
+                                        <TooltipProvider delayDuration={100}>
+                                            <Tooltip>
+                                                <TooltipTrigger
+                                                    className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                    <BsArrowUpRight
+                                                        className="text-white text-3xl group-hover:text-accent"/>
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>Visit website</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </Link>
+                                )}
+                                <Link href={projects[projectIndex].github} target="_blank">
+                                    <TooltipProvider delayDuration={100}>
+                                        <Tooltip>
+                                            <TooltipTrigger
+                                                className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                <BsGithub className="text-white text-3xl group-hover:text-accent"/>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>View source</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="w-full xl:w-[50%]">
-                    <Swiper
-                        spaceBetween={30}
-                        slidesPerView={1}
-                        className="xl:h-[520px] mb-12"
-                        onSlideChange={(swiper) => setProject(projects[swiper.activeIndex])}
-                    >
-                        {projects.map((project, index) => (
-                            <SwiperSlide key={index} className="w-full">
-                                <div className="h-[460px] relative group flex justify-center items-center">
-                                    {/* Project overlay */}
-                                    {/*<div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"/>*/}
-                                    {/* Project media */}
-                                    <div className="relative w-full h-full">
+                    <div className="w-full xl:w-[50%] flex justify-center items-center relative">
+                        <Swiper
+                            spaceBetween={30}
+                            slidesPerView={1}
+                            className="xl:h-[520px] mb-12 flex justify-center items-center"
+                            onSlideChange={(swiper) => setProjectIndex(swiper.activeIndex)}
+                        >
+                            {projects.map((project, index) => (
+                                <SwiperSlide key={index} className="w-full flex justify-center items-center">
+                                    <div className="relative w-full h-full flex justify-center items-center">
                                         {project.image.endsWith('.mp4') ? (
                                             <video
                                                 src={project.image}
@@ -145,28 +155,39 @@ const Projects = () => {
                                                 className="object-center object-contain w-full h-full"
                                             />
                                         ) : (
-                                            <Image
-                                                src={project.image}
-                                                alt={project.title}
-                                                layout="fill"
-                                                objectFit="contain"
-                                                className="object-center object-contain"
-                                            />
+                                            <div className="flex justify-center items-center w-full h-full">
+                                                <div style={{
+                                                    width: imageDimensions[index].width,
+                                                    height: imageDimensions[index].height,
+                                                    display: 'flex',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center'
+                                                }}>
+                                                    <Image
+                                                        src={project.image}
+                                                        alt={project.title}
+                                                        layout="intrinsic"
+                                                        width={imageDimensions[index].width}
+                                                        height={imageDimensions[index].height}
+                                                        objectFit="contain"
+                                                        className="object-center object-contain"
+                                                    />
+                                                </div>
+                                            </div>
                                         )}
                                     </div>
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                        {/* Swiper navigation */}
-                        <WorkSliderBtns
-                            containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                            btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
-                        />
-                    </Swiper>
+                                </SwiperSlide>
+                            ))}
+                            <WorkSliderBtns
+                                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                            />
+                        </Swiper>
+                    </div>
                 </div>
             </div>
-        </div>
-    </motion.section>
+        </motion.section>
+    );
 };
 
 export default Projects;
